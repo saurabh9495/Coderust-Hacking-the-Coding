49e9a29a4b32343f58b2cc00bff906344cff316a
Saurabh Kumar <saurabhkumar@Saurabhs-MacBook-Air.local>
2022-06-03 05:40:10 +0530

    Implement Binary Search on a Sorted Array

---
+++ Arrays/Implement Binary Search on a Sorted Array.py
@@ -0,0 +1,15 @@
+def binary_search(nums,target):
+    if not nums:
+        return -1
+    start = 0
+    end = len(nums) - start - 1
+    mid = (start + end) // 2
+    while start <= end:
+        mid = (start + end) // 2
+        if target == nums[mid]:
+            return mid
+        elif target < nums[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
