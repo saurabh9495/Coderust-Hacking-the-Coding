0a3584516ad56712a88e0ebac1514300e4f9a375
Saurabh Kumar <saurabhkumar@Saurabhs-MacBook-Air.local>
2022-06-03 05:48:19 +0530

    Rotate an Array by N Elements

---
+++ Arrays/Rotate an Array by N Elements.py
@@ -0,0 +1,15 @@
+def reverse_array(nums,start,end):
+    while start < end:
+        nums[start],nums[end] = nums[end],nums[start]
+        start += 1
+        end -= 1    
+
+def rotate_array(nums,n):
+    n = n % len(nums)
+    if n < 0:
+        n+= len(nums)
+    start = 0
+    end = len(nums) - 1
+    reverse_array(nums,0,len(nums)-1)
+    reverse_array(nums,0,n-1)
+    reverse_array(nums,n,len(nums)-1)
